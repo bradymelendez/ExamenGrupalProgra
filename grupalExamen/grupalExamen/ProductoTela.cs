04adb4766bb266044dcc413e2e15874e982191a2
DayDaysi <roxashola@gmail.com>
2023-09-30 14:50:07 -0500

    Added Producto Tela

---
+++ grupalExamen/grupalExamen/ProductoTela.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grupalExamen
+{
+    class ProductoTela : Producto
+    {
+        public string Tamaño { get; set; }
+        public string Material { get; set; }
+        public string Color { get; set; }
+        public ProductoTela(string nombre, float precio, int stock, string tamaño, string material, string color)
+        {
+            Nombre = nombre;
+            Precio = precio;
+            Stock = stock;
+            Tamaño = tamaño;
+            Material = material;
+            Color = color;
+        }
+        public override string ObtenerCaracteristicas()
+        {
+            return $"Nombre: {Nombre}, Tamaño: {Tamaño}, Material: {Material}, Color: {Color}";
+        }
+    }
+}
